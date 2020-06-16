fffd5083cc7c6606ec2510878360b437d4eca2bf
Tiago Correia <tiagocorreiia@gmail.com>
2020-06-16 16:06:37 -0300

    Initial Commit

---
+++ src/routes/carros.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const CarroDB = require('../models/CarroDB')
+
+// Busca Todos os Carros
+router.get('/', (req, res, next) => {
+  CarroDB.getCarros((error, carros) => {
+    if (error) {
+      console.log(`Erro de SQL: ${error.message}`)
+      return next(error)
+    }
+    res.json(carros)
+  })
+})
+
+// Busca Carro Por ID
+router.get('/:id(\\d+)', (req, res) => {
+  const id = req.params.id
+  CarroDB.getCarroById(id, (carro) => {
+    res.json(carro)
+  })
+})
+
+// Busca Carros Por Tipo
+router.get('/:tipo', (req, res) => {
+  const tipo = req.params.tipo
+  CarroDB.getCarrosByType(tipo, (carros) => {
+    res.json(carros)
+  })
+})
+
+// Cria Novo Carro
+router.post('/', (req, res) => {
+  const carro = req.body
+  CarroDB.insertCarro(carro, (carro) => {
+    res.json(carro)
+  })
+})
+
+// Atualiza Carro
+router.put('/', (req, res) => {
+  const carro = req.body
+  CarroDB.updateCarro(carro, (carro) => {
+    res.json({ msg: 'Carro atualizado com sucesso' })
+  })
+})
+
+// Deleta Um Carro
+router.delete('/:id(\\d+)', (req, res) => {
+  const id = req.params.id
+  console.log(`Deletar carro: ${id}`)
+  CarroDB.deleteCarroById(id, (affectedRows) => {
+    res.json({ msg: 'Carro deletado com sucesso' })
+  })
+})
+
+module.exports = router
